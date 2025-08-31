a4601a692aa300f24b0fdfbc09a97cfc76b309c4
BILAL HUSSAIN <billhssn777@icloud.com>
2025-08-31 05:43:50 -0700

    Add migrations and models for Suppliers, Units, Tags, and Allergies

---
+++ database/migrations/2025_08_31_123945_create_suppliers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100)->unique();
+            $table->text('address')->nullable();
+            $table->text('preferred_items')->nullable(); // JSON or comma-separated
+            $table->string('contact', 100)->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+
+};
