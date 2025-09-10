e8a4318c8674bbcd5fc37d1343367ebcc6809982
Junaid-Shiekh <junaid.khan.jan143@gmail.com>
2025-09-10 11:12:31 +0500

    migration changes

--- database/migrations/2025_09_03_110100_create_stock_entries_table.php
+++ database/migrations/2025_09_03_110100_create_stock_entries_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->foreignId('category_id')
-                ->constrained('categories')
+                ->constrained('inventory_categories')
                 ->cascadeOnDelete();
 
             $table->foreignId('supplier_id')
