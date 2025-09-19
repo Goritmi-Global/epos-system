17f7b737d6b380cd0a47919abc8255bac12aa81c
KashifKhan456 <kashifkhan4unoor@gmail.com>
2025-09-19 17:07:56 +0500

    Make some adjustment to MultiSelect and other validation errors

--- resources/js/Pages/Backend/Inventory/PurchaseOrder/PurchaseComponent.vue
+++ resources/js/Pages/Backend/Inventory/PurchaseOrder/PurchaseComponent.vue
@@ -484,3 +484,43 @@ const p_supplier = ref(null);
         </div>
     </div>
 </template>
+
+<style scoped>
+/* ====================Select Styling===================== */
+/* Entire select container */
+:deep(.p-select) {
+  background-color: white !important;
+  color: black !important;
+  border-color: #9b9c9c
+}
+
+/* Options container */
+:deep(.p-select-list-container) {
+  background-color: white !important;
+  color: black !important;
+}
+
+/* Each option */
+:deep(.p-select-option) {
+  background-color: transparent !important; /* instead of 'none' */
+  color: black !important;
+}
+
+/* Hovered option */
+:deep(.p-select-option:hover) {
+  background-color: #f0f0f0 !important;
+  color: black !important;
+}
+
+/* Focused option (when using arrow keys) */
+:deep(.p-select-option.p-focus) {
+  background-color: #f0f0f0 !important;
+  color: black !important;
+}
+:deep(.p-select-label){
+    color: #000 !important;
+}
+:deep(.p-placeholder){
+    color: #80878e !important;
+}
+</style>
