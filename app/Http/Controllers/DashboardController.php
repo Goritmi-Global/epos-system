f1c24814fa1f10646f1455434deaa79decdd8891
BILAL HUSSAIN <billhssn777@icloud.com>
2025-10-01 15:07:48 +0500

    updated some desingn changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,10 +13,6 @@ class DashboardController extends Controller
 
         // If profile missing, send to onboarding
         $hasProfile = RestaurantProfile::where('user_id', $user->id)->exists();
-<<<<<<< HEAD
-        // dd($user,$hasProfile);
-=======
->>>>>>> 631ddb31a5cf15836eec985b80d7a9c317cdf80b
         if (! $hasProfile) {
             // optional: make extra-sure no stale intended redirect lingers
             session()->forget('url.intended');
