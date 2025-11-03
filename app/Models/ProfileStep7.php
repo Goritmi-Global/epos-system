e6ed7554fe3bc9b901774333aaacdf9decfcf8db
Junaid-Shiekh <junaid.khan.jan143@gmail.com>
2025-11-03 22:45:17 +0500

    Database backup

--- app/Models/ProfileStep7.php
+++ app/Models/ProfileStep7.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -6,10 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class ProfileStep7 extends Model
 {
     protected $table = 'profile_step_7';
-    protected $fillable = ['user_id','cash_enabled','card_enabled','integrated_terminal','custom_payment_options','default_payment_method'];
+
+    protected $fillable = [
+        'user_id',
+        'cash_enabled',
+        'card_enabled',
+        'integrated_terminal',
+        'custom_payment_options',
+        'default_payment_method',
+        'logout_after_order',
+        'logout_after_time',
+        'logout_manual_only',
+        'logout_time_minutes',
+    ];
+
     protected $casts = [
         'custom_payment_options' => 'array',
         'cash_enabled' => 'boolean',
         'card_enabled' => 'boolean',
+        'logout_after_order' => 'boolean',
+        'logout_after_time' => 'boolean',
+        'logout_manual_only' => 'boolean',
     ];
 }
